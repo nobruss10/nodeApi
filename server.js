01423efe262fe504d2c057ab6567a60c0fad7849
Bruno santos <bruno@sequenza.com.br>
2018-11-11 19:45:04 -0200

    Isolando as configurações do express

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-const express = require('express');
+const app = require('./config/express')();
 const mongoose = require('mongoose');
-const cors = require('cors');
 const requireDir = require('require-dir');
-
-const app = express();
-app.use(express.json());
-app.use(cors());
 
 mongoose.connect(
     'mongodb://localhost:27017/nodeapi',
